6a2306cf61a8219f93d72074e4c19119770f19ca
Ivan Veramyou <ivan.veramyou22@gmail.com>
2023-11-01 16:46:36 +0100

    Initial project setup

---
+++ mobile-secure-pix/Views/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-secure-pix
+//
+//  Created by Ivan Veramyou on 1.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ mobile-secure-pixUITests/mobile_secure_pixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mobile_secure_pixUITestsLaunchTests.swift
+//  mobile-secure-pixUITests
+//
+//  Created by Ivan Veramyou on 1.11.23.
+//
+
+import XCTest
+
+final class mobile_secure_pixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
