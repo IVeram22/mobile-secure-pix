05941273000bd266b04ec1ef2e84f78b6a75373d
Ivan Veramyou <ivan.veramyou22@gmail.com>
2023-11-05 19:17:26 +0100

    Added UINavigationController for HomeViewController

---
+++ mobile-secure-pix/Helpers/Router/Add/AddRouter.swift
@@ -0,0 +1,13 @@
+//
+//  AddRouter.swift
+//  mobile-secure-pix
+//
+//  Created by Ivan Veramyou on 5.11.23.
+//
+
+import Foundation
+
+protocol AddRouter: AnyObject {
+    func addImage()
+    func addFolder()
+}
