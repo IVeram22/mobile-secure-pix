c49edec98bc02dda399a329432c6e3de86687bd4
Ivan Veramyou <ivan.veramyou22@gmail.com>
2023-11-09 09:22:35 +0100

    Fixed ImagesPresenter.swift

--- mobile-secure-pix/Presenters/ImagesPresenter.swift
+++ mobile-secure-pix/Presenters/ImagesPresenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class ImagesPresenter {
-    private var imagesInputPresenter: ImagesInputPresenter?
+    private weak var imagesInputPresenter: ImagesInputPresenter?
     private let manager = ImageDataManager()
     
     func setImagesInputPresenter(with delegate: ImagesInputPresenter) {
